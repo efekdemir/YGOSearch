a3fb0da4067d4e4ea17c55b9f66c3d99dc96747a
“[Efe <efedemir2001@gmail.com>
2024-09-05 17:57:56 -0700

    finished project??!

--- YGOSearch/View/Main Menu/EfeDemir.swift
+++ YGOSearch/View/Main Menu/EfeDemir.swift
@@ -6,49 +6,60 @@
 //
 
 import SwiftUI
+import AnimatedGradient
 
 struct EfeDemir: View {
     var body: some View {
-        VStack {
-            Image("EfeDemir")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 200, height: 300)
-            Text("Hello, World!")
-                .padding()
-                .font(.headline)
-            Text("Thank you for downloading YGO Search!")
-                .padding(.horizontal)
-                .font(.subheadline)
-            
-            Text("If you have any recommendations for improving the app, feel free to contact me :)")
-                .padding(.horizontal)
-                .font(.subheadline)
-            
+        ZStack {
+            AnimatedLinearGradient(colors: [.blue, .white])
+                .numberOfSimultaneousColors(3)
+                .setAnimation(.linear(duration: 2))
+                .gradientPoints(start: .bottomLeading, end: .topTrailing)
             VStack {
-                Link(destination: URL(string: "https://www.linkedin.com/in/efekdemir/")!) {
-                    Text("Connect on LinkedIn")
-                        .foregroundColor(.white)
-                        .padding()
-                        .background(Color.blue)
-                        .cornerRadius(8)
+                Image("EfeDemir")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 300)
+                
+                Text("Hello, World!")
+                    .padding()
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .glowBorder(color: .black, lineWidth: 3)
+                
+                Text("Thank you for downloading YGO Search!")
+                    .padding(.horizontal)
+                    .padding(.bottom)
+                    .font(.subheadline)
+                    .foregroundColor(.white)
+                    .glowBorder(color: .black, lineWidth: 2)
+                
+                Text("If you have any recommendations for improving the app, feel free to contact me :)")
+                    .padding(.horizontal)
+                    .font(.subheadline)
+                    .foregroundColor(.white)
+                    .glowBorder(color: .black, lineWidth: 2)
+                
+                VStack {
+                    Link(destination: URL(string: "https://www.linkedin.com/in/efekdemir/")!) {
+                        Text("Connect on LinkedIn")
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(Color.blue)
+                            .cornerRadius(8)
+                    }
+                    .padding(.top, 20)
+                    
+                    Link(destination: URL(string: "https://github.com/efekdemir")!) {
+                        Text("Visit my GitHub")
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(Color.black)
+                            .cornerRadius(8)
+                    }
+                    .padding(.top, 10)
                 }
-                .padding(.top, 20)
-
-                Link(destination: URL(string: "https://github.com/efekdemir")!) {
-                    Text("Visit my GitHub")
-                        .foregroundColor(.white)
-                        .padding()
-                        .background(Color.black)
-                        .cornerRadius(8)
-                }
-                .padding(.top, 10)
             }
         }
     }
-}
-
-
-#Preview {
-    EfeDemir()
 }
